d872187419ef58f442fee0cb6888fbce6a21014e
Olga Shiryaeva <oshiryaeva@protonmail.com>
2022-01-04 10:35:47 -0600

    BFS solution

--- Solutions/bfs_example.js
+++ Solutions/bfs_example.js
@@ -6,9 +6,35 @@ const mangoSellersGraph = {
   anuj: [],
   peggy: [],
   thom: [],
-  jonny,
+  jonny: [],
 }
 
-function bfs() {
-
+const isMangoSeller = (sellerName) => {
+  const mangoSellers = ['jonny'];
+  return mangoSellers.includes(sellerName);
 }
+
+// Determine if the given node has a connection to a node
+// that is a mango seller
+// returns name of the node if it does
+// returns null otherwise
+function bfs(graph, root) {
+  let queue = graph[root];
+  let visited = [];
+
+  while (queue.length > 0) {
+    let currentNode = queue.shift();
+
+    if (!visited.includes(currentNode)) {
+      if (isMangoSeller(currentNode)) {
+        return currentNode;
+      } else {
+        visited.push(currentNode);
+        queue.push(...graph[currentNode]);
+      }
+    }
+  }
+  return null;
+}
+
+console.log(bfs(mangoSellersGraph, 'anuj'));
