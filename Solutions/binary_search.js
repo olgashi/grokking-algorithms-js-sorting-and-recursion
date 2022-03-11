fe1096c1a3027df14f8dcfc1df21687ae4d36b41
Olga <41551585+olgashi@users.noreply.github.com>
2022-03-11 16:58:09 -0600

    Delete comments

--- Solutions/binary_search.js
+++ Solutions/binary_search.js
@@ -1,51 +1,8 @@
 const arr = [1, 5, 7, 12, 44, 45, 49, 52, 55, 67, 88, 90, 91, 100];
 
-// find numbers
-const num1 = 45; // 5
-const num2 = 100; // 13
-const num3 = 69; // null
-
-/*
-P
-Input:
-- sorted array
-- element to search
-
-Output:
-- index of the element to search in array
-- null if element is not found
-
-Examples:
-binarySearch(num1); // 5
-binarySearch(num2); // 13
-binarySearch(num1); // null
-
-Data Structure:
-- n/a
-
-Algorithm:
-- binary search
-
-- determine the middle element:
-    arrCopy.length / 2, rounded down - save to variable middle
-
-- create variable lower - set to 0
-- create variable upper - set to arrCopy.length - 1
-
-while lower <= upper
-
-- if num === arrCopy[middle]
-  return middle;
-
-if num < arrCopy[middle]
-  upper = middle - 1
-else 
-  lower = middle + 1
-determine middle element  as Math.ceil((lower + upper) / 2), set to middle
-
-return null;
-Code:
-*/
+const num1 = 45;
+const num2 = 100;
+const num3 = 69;
 
 const binarySearch = function(arr, num) {
   let middle = Math.ceil(arr.length / 2);
